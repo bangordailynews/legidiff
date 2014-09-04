be4c8e400ae3288e76dd562b0118c8a115ee040a
Garrett Wilkin <garrett.wilkin@gmail.com>
2014-09-04 19:13:00 +0000

    add diff template which will display the difference between two versions of a bill.

--- bill/views/diff.py
+++ bill/views/diff.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 # No Models in use yet.
 
 def diff(request):
-    output = []
-    return HttpResponse(output)
+    diff = ""
+    data = {"title": "diff of a bill",
+            "bill": { "id": "HP 0718",
+                "description": "An Act Regarding the Swans Island Lobster Fishing Zone"},
+            "before": { "version": "original"},
+            "after": {"version": "first revision"},
+            "diff": diff
+            }
+    return render_to_response('diff.jinja', data)
